326aef106442f23536b382824dad913e68f5fe13
Ishan Pandey <ishpan2@Ishans-MacBook.local>
2017-05-09 12:05:36 -0700

    Fully functional timer

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var totalBillLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func updateTip(_ sender: Any) {
+        let tips = [0.1, 0.15, 0.2]
+        let price = Double(billField.text!) ?? 0
+        let tip = price*tips[tipControl.selectedSegmentIndex]
+        let total = price + tip
+        tipLabel.text = "Tip: " + String(format: "$%.2f", tip)
+        totalBillLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
